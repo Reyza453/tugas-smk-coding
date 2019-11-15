9aa7468c3f02ccc082a63d6fcee92b70db8c3285
reyza453 <gomugomu3885@gmail.com>
2019-11-16 06:02:01 +0700

    update list_bis_driver

---
+++ app/src/main/java/com/example/tugassmkcodng/list_bis_driver.kt
@@ -0,0 +1,31 @@
+package com.example.tugassmkcodng
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_list_bis_driver.*
+
+class list_bis_driver : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_bis_driver)
+
+        btn_img_info1.setOnClickListener({
+            val Intent1=Intent(this,list_showmap_bus_driver::class.java)
+            startActivity(Intent1)
+        })
+        btn_img_info2.setOnClickListener({
+            val Intent2=Intent(this,list_showmap_bus_driver::class.java)
+            startActivity(Intent2)
+        })
+        btn_img_info3.setOnClickListener({
+            val Intent3=Intent(this,list_showmap_bus_driver::class.java)
+            startActivity(Intent3)
+        })
+        btn_img_info4.setOnClickListener({
+            val Intent4=Intent(this,list_showmap_bus_driver::class.java)
+            startActivity(Intent4)
+        })
+    }
+}
